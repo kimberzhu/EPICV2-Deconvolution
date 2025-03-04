b72600fe58f41f98b8f6625818223e68ba556e17
Kim Zhu <131017053+kimberzhu@users.noreply.github.com>
2025-03-04 11:24:20 -0800

    Update process_array_EpicV2.R

--- process_array_EpicV2.R
+++ process_array_EpicV2.R
@@ -53,9 +53,9 @@ process_array <- function(array_dir, out_path, ref_sample_path=args[3])
 	
 	print(paste0("1. process_array script - READING ARRAY - ",Sys.time()))
 
-    	# Select samples from whole blood #
+    	# Select cfDNA samples 
     	targets_pre <- read.csv("/u/scratch/k/kitzhu/TargetPhenos_Full.csv", header=T) %>% 
-        filter(Tissue == "Blood")
+        filter(Tissue == "cfDNA")
     	write.csv(targets_pre, file = file.path(array_dir, "targets.csv"), row.names = FALSE)
 
 	# Read in Sample Sheet
